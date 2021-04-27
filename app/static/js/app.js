cef288b07259966a5039e86b4c048c3c4036b009
danichol <daneelnichol@outlook.com>
2021-04-26 19:56:30 -0500

    Final Changes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,34 +1,5 @@
 /* Add your Application JavaScript */
 // Instantiate our main Vue Instance
-const app = Vue.createApp({
-    data() {
-        return {}    
-    }, 
-    components: {
-        'Home': Home,
-        'upload-form': uploadForm
-    }
-});
-
-app.component('app-header', {
-    name: 'AppHeader',
-    template: `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-      <a class="navbar-brand" href="#">Lab 7</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    `
-});
 
 const Home = {
     name: 'Home',
@@ -42,6 +13,7 @@ const Home = {
         return {}
     }
 };
+
 
 const uploadForm ={
     name: 'upload-form',
@@ -72,7 +44,7 @@ const uploadForm ={
                 <label> Description: </label>
                 <textarea name="description" class="form-control"></textarea>
                 <label> Photo: </label>
-                <input type="file" name="photo"></input>  
+                <input type="file" name="photo">  
             </div>
         <button class="btn btn-primary mb-2">Submit</button>
         </form>
@@ -126,6 +98,45 @@ const uploadForm ={
         }
     },
 }; 
+
+
+
+
+
+const app = Vue.createApp({
+    data() {
+        return {}    
+    }, 
+    components: {
+        'Home': Home,
+        'upload-form': uploadForm
+    }
+});
+
+app.component('app-header', {
+    name: 'AppHeader',
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <a class="navbar-brand" href="#">Lab 7</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/upload">Upload<span class ="sr-only>(current)</span></router-link>
+          </li>  
+        </ul>
+      </div>
+    </nav>
+    `
+});
+
+
 
 
 
